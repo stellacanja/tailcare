788c24a50ef0feba253d980a35d06f0db47facbd
jericholuna <jrchcbrl@gmail.com>
2025-04-08 16:36:40 +0800

    added Login

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
+
+// Vuetify
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
---
+++ src/views/SignupView.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from 'vue'
+import imgFour from '@/assets/images/four.png'
+import imgWel from '@/assets/images/welcome.png'
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app>
+      <!-- Centered App Bar Logo -->
+      <v-app-bar class="navbar" flat height="150" style="background-color: #f4f5f7">
+        <v-container class="d-flex justify-center align-center fill-height pa-0">
+          <div class="d-flex flex-column align-center" style="line-height: 1; margin-top: 4px">
+            <img :src="imgWel" alt="TailCare Logo" style="height: 150px; object-fit: contain" />
+          </div>
+        </v-container>
+      </v-app-bar>
+
+      <br />
+      <br />
+
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col cols="5" md="5">
+              <br />
+              <br />
+              <v-card class="pa-4">
+                <h1 id="welc" style="color: #8c52ff"><b>WELCOME OWNERS</b></h1>
+                <br />
+                <v-form fast-fail @submit.prevent>
+                  <v-text-field label="Email address"></v-text-field>
+
+                  <v-text-field label="Password"></v-text-field>
+
+                  <v-btn class="mt-2" type="submit" style="background-color: skyblue" block
+                    >Submit</v-btn
+                  >
+                </v-form>
+              </v-card>
+              <br />
+              <h3>Don't have an account? <link rel="" href="" />Sign up</h3>
+            </v-col>
+            <v-col cols="7" md="7">
+              <img
+                :src="imgFour"
+                alt="Four Pets"
+                class="card-img-top"
+                style="height: 500px; object-fit: cover; width: 800px"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer border app id="foot" class="justify-center" style="background-color: #f4f5f7"
+        >TailCare@2025</v-footer
+      >
+    </v-app>
+  </v-responsive>
+</template>
