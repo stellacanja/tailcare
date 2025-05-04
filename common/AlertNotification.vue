f0dd020e95c5f111063de2292d4d351d476570f8
kryzel03 <kryzelricaebalebulagsac@gmail.com>
2025-05-04 10:30:24 +0800

    feat:added registration func

---
+++ common/AlertNotification.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps(['formSuccessMessage', 'formErrorMessage'])
+</script>
+
+<template>
+  <v-alert
+    v-if="props.formSuccessMessage"
+    :text="props.formSuccessMessage"
+    title="Success!"
+    type="Success"
+    color="success"
+    icon="$success"
+    variant="tonal"
+    density="compact"
+    border="Start"
+    Closable
+  ></v-alert>
+
+  <v-alert
+    v-if="props.formErrorMessage"
+    :text="props.formErrorMessage"
+    title="Ooops!"
+    type="error"
+    color="error"
+    icon="$error"
+    variant="tonal"
+    density="compact"
+    border="Start"
+    Closable
+  ></v-alert>
+</template>
